9067bef10bbe7378530bbcffefda2d9e797e8e59
AWM <digustivus@gmail.com>
2026-06-27 06:32:05 -0300

    Add script to apply execute permissions to .sh files

---
+++ fix-permisos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "=== Aplicando permisos ==="
+
+chmod +x /home/ani/compartido/amor/*.sh
+echo "✅ Permisos +x aplicados a todos los .sh en la carpeta"
+
+ls -l /home/ani/compartido/amor/*.sh
