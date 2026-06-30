514e0c91f1c36db1d944beab241a9b8ce3e73000
awmorante <digustivus@gmail.com>
2026-06-30 14:41:58 -0300

    limpiando archivos que quizas sean mas para otro repo asi separo y ordeno

--- hola_mundo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "========================================"
-echo "¡Hola Mundo!"
-echo "========================================"
-echo "Hora actual: $(date '+%Y-%m-%d %H:%M:%S')"
-echo "Dirección IP: $(hostname -I | awk '{print $1}')"
-echo "========================================"
